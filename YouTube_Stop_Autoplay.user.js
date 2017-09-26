82c40f244eada5915ff823c6cb83d66388494ab7
angel9215 <angel9215@hotmail.com>
2017-09-26 17:35:42 -0500

    Add files via upload

---
+++ YouTube_Stop_Autoplay.user.js
@@ -0,0 +1,15 @@
+﻿// ==UserScript==
+// @name        YouTube: Remove Autoplay
+// @namespace   youtubeRemoveAutoplay
+// @version     1.0
+// @include     http://*youtube.com/*
+// @include     https://*youtube.com/*
+// @exclude     https://*.youtube.com/embed*
+// @grant       none
+// ==/UserScript==
+
+document.addEventListener('spfdone', function() {
+	document.getElementById('autoplay-checkbox').parentElement.remove();
+});
+
+document.getElementById('autoplay-checkbox').parentElement.remove();
